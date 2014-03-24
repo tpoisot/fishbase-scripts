d64ce16a4db9342138f59db69a21d90626a2cfb3
Timothée Poisot <t.poisot@gmail.com>
2014-03-24 12:40:10 -0400

    Updated the scripts to run with the revised API

--- scripts/get_species_by_id.jl
+++ scripts/get_species_by_id.jl
@@ -11,10 +11,7 @@ for sp_idx in species_id
    request = get(request_url)
    if request.status == 200
       request_content = request.data
-      request_text = JSON.parse(request.data)
-      if length(request_text) == 1
-         response = request_text[1]
-         println("#", sp_idx, "\t", response["Genus"], " ", response["Species"])
-      end
+      response = JSON.parse(request.data)
+      println("#", sp_idx, "\t", response["Genus"], " ", response["Species"])
    end
 end
--- scripts/get_species_by_id.py
+++ scripts/get_species_by_id.py
@@ -8,6 +8,5 @@ for SP in SPECIES_ID:
     request_url = FB_API_ROOT + 'species/id/' + str(SP)
     r = re.get(request_url)
     if r.status_code == 200 :
-        if len(r.json()) == 1 :
-            record = r.json()[0] # This will become unecessary later
-            print "#" + str(SP) + "\t" + record['Genus'] + ' ' + record['Species']
+        record = r.json()
+        print "#" + str(SP) + "\t" + record['Genus'] + ' ' + record['Species']
